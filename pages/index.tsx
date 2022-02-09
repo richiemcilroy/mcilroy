78aad29e317c0397e7092931ae766b68918197e2
Richie McIlroy <richie@mcilroy.co>
2022-02-09 22:26:16 +0000

    Go live

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,81 +2,52 @@ import Head from 'next/head'
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div className="bg-gray-50 flex min-h-screen flex-col items-center justify-center py-2">
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <meta charSet="utf-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="description" content="Hey, I'm Richie! I'm a Software Engineer 👨‍💻 Frenchie Dad 🐶 Evertonian 😢" />
+
+        {/* Twitter */}
+        <meta name="twitter:card" content="summary_large_image" key="twcard" />
+        <meta name="twitter:image" content="https://mcilroy.co/og.png"/>
+
+        {/* Open Graph */}
+        <meta property="og:url" content="https://mcilroy.co" key="ogurl" />
+        <meta property="og:image" content="https://mcilroy.co/og.png" key="ogimage" />
+        <meta property="og:site_name" content="Richie McIlroy" key="ogsitename" />
+        <meta property="og:title" content="👋 Richie McIlroy" key="ogtitle" />
+        <meta property="og:description" content="Hey, I'm Richie! I'm a Software Engineer 👨‍💻 Frenchie Dad 🐶 Evertonian 😢" key="ogdesc" />
+        <title>👋 Richie McIlroy</title>
       </Head>
 
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
-            pages/index.tsx
-          </code>
-        </p>
-
-        <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+      <main className="px-4">
+        <div className="text-center">
+          <img className="w-36 h-auto mx-auto mb-3 border-4 border-gray-200 rounded-full hover:rotate-6 transition-all" src="/me.png" alt="Richie McIlroy"/>
+          <h1 className="text-3xl md:text-5xl font-bold mb-2 text-gray-800">Hey, I'm Richie!</h1>
+          <h2 className="text-xl md:text-2xl font-medium text-gray-500">Software Engineer 👨‍💻 Frenchie Dad 🐶 Evertonian 😢</h2>
+          <div className="flex items-center justify-center mt-7">
+            <a 
+              href="https://twitter.com/richiemcilroy"
+              className="px-3 py-2 bg-blue-500 hover:bg-blue-600 text-white text-sm rounded-lg font-semibold mx-2 hover:rotate-2 transition-all"
+            >
+              Twitter
+            </a>
+            <a 
+              href="https://github.com/richiemcilroy"
+              className="px-3 py-2 bg-gray-800 hover:bg-gray-900 text-white text-sm rounded-lg font-semibold mx-2 hover:-rotate-2 transition-all"
+            >
+              GitHub
+            </a>
+            <a 
+              href="https://www.linkedin.com/in/richiemcilroy/"
+              className="px-3 py-2 bg-blue-900 hover:bg-blue-800 text-white text-sm rounded-lg font-semibold mx-2 hover:rotate-2 transition-all"
+            >
+              LinkedIn
+            </a>
+          </div>
         </div>
       </main>
-
-      <footer className="flex h-24 w-full items-center justify-center border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="ml-2 h-4" />
-        </a>
-      </footer>
     </div>
   )
 }
